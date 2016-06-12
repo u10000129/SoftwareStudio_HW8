fe2f7623ed1f1060e1f44388ee7a9d42c0ff98e1
u10000129 <u10000129@gmail.com>
2016-06-12 17:39:08 +0800

    add Server.java

--- server/src/main/java/com/example/Main.java
+++ server/src/main/java/com/example/Main.java
@@ -1,4 +1,14 @@
 package com.example;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        Server obj = new Server(frame);
+    }
 }
---
+++ server/src/main/java/com/example/Server.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.*;
+
+/**
+ * Created by NTHUCS on 2016/6/12.
+ */
+public class Server implements Runnable{
+    private Thread thread;
+    private ServerSocket servSock;
+    private JFrame parent;
+    private JTextArea textArea;
+
+    public Server(JFrame f){
+        this.parent = f;
+        this.textArea = new JTextArea();
+        try {
+            // Detect server ip
+            InetAddress IP = InetAddress.getLocalHost();
+            System.out.println("IP of my system is := "+IP.getHostAddress());
+            textArea.append("IP of my system is := "+IP.getHostAddress());
+            System.out.println("Waitting to connect......");
+            textArea.append("Waitting to connect......");
+
+            // Create server socket
+            servSock = new ServerSocket(2000);
+
+            // Create socket thread
+            thread = new Thread(this);
+            thread.start();
+        } catch (java.io.IOException e) {
+            System.out.println("Socket啟動有問題 !");
+            System.out.println("IOException :" + e.toString());
+        } finally{
+
+        }
+    }
+
+    @Override
+    public void run(){
+        // Running for waitting multiple client
+        while(true){
+            try{
+                // After client connected, create client socket connect with client
+                Socket clntSock = servSock.accept();
+                InputStream in = clntSock.getInputStream();
+
+                System.out.println("Connected!!");
+                this.textArea.append("Connected!!");
+
+                // Transfer data
+                byte[] b = new byte[1024];
+                int length;
+
+                length = in.read(b);
+                String s = new String(b);
+                System.out.println("[Server Said]" + s);
+                this.textArea.append("[Server Said]" + s);
+            }
+            catch(Exception e){
+                //System.out.println("Error: "+e.getMessage());
+            }
+        }
+    }
+}
